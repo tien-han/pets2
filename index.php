626e2c2e0a1822e084ec2e392491ea3507625a3f
Tien Han <tienthuyhan@gmail.com>
2024-04-20 17:40:10 -0400

    Add in composer, index file, and gitignore from Pets 1

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 328/pets/index.php
+ * Simple MVC using the Fat-Free framework.
+ * @author Tien Han
+ * @version 1.0
+ */
+
+    //Turn on error reporting
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    //Require the autoload file
+    require_once('vendor/autoload.php');
+
+    //Instantiate the F3 Base class (Fat-Free)
+    $f3 = Base::instance();
+
+    //Define a default route
+    $f3-> route('GET /', function() {
+        //Render a view page
+        $view = new Template();
+        echo $view->render('views/home.html');
+    });
+
+    //Run Fat-Free
+    $f3->run();
+?>
